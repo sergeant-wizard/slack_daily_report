89119376c22f35e13398abb0e79893f7eb00b5ac
Ryo Miyajima <sergeant.wizard@gmail.com>
2020-04-05 09:23:56 +0900

    markdown

--- daily_report.py
+++ daily_report.py
@@ -1,41 +1,64 @@
 import datetime
+import json
 import os
 
 import slack
 
 PDT = -7
 JST = +9
-time_difference = JST - PDT
+hour = 9
+minute = 24
+channel = '#rpt-ryo'
 
+time_difference = JST - PDT
 # FIXME
 time_difference = 0
-
 current_timestamp = datetime.datetime.today()
-timestamp = int(datetime.datetime(
-    current_timestamp.year,
-    current_timestamp.month,
-    current_timestamp.day,
-    8,
-    52,
-).timestamp())
-timestamp += 60 * 60 * time_difference
+
+
 text = f"""
 Ryo's daily report for:
 {current_timestamp.year}/{current_timestamp.month}/{current_timestamp.day}
 
 Do:
-- hoge
+
+* hogehoge
+
 Did:
-- bar
+
+* bar
+
 """
-client = slack.WebClient(token=os.environ['SLACK_AUTH_CODE'])
-response = client.chat_scheduleMessage(
-    channel='#rpt-ryo',
-    text={
-        'type': 'mrkdwn',
-        'text': text,
-    },
-    post_at=timestamp,
-)
-assert response['ok']
-assert response['message']['text'] == text
+
+
+def create_timestamp():
+    timestamp = int(datetime.datetime(
+        current_timestamp.year,
+        current_timestamp.month,
+        current_timestamp.day,
+    hour, minute,
+    ).timestamp())
+    return timestamp + 60 * 60 * time_difference
+
+
+def main():
+    client = slack.WebClient(token=os.environ['SLACK_AUTH_CODE'])
+    response = client.api_call(
+        'chat.scheduleMessage',
+        json={
+            'channel': channel,
+            'blocks': json.dumps([{
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': text,
+                },
+            }]),
+            'post_at': create_timestamp(),
+        }
+    )
+    assert response['ok']
+
+
+if __name__ == '__main__':
+    main()
